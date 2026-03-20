f763136ab5ff62d35b286ccbea391e0406ec16ae
Isaac Reis <isaacreis209@gmail.com>
2026-03-19 21:43:12 -0300

    correct hitbox
    add one character, solider 2d

--- AshenKatana/Assets/Scripts/BasicMovimentPlayer.cs
+++ AshenKatana/Assets/Scripts/BasicMovimentPlayer.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMovimentPlayer : MonoBehaviour {
+public class BasicMovimentPlayer : MonoBehaviour
+{
     private float horizontalInput;
     private Rigidbody2D rb;
-
     [SerializeField] private int speed = 5;
-
-
     [SerializeField] private Transform GroundChecker;
     [SerializeField] private LayerMask GroundLayer;
+    private bool facingRight = true;
+
 
 
     private bool isGrounded;
@@ -27,24 +27,28 @@ public class BasicMovimentPlayer : MonoBehaviour {
     [SerializeField] private AudioClip[] stepsAudioClip;
     [SerializeField] private AudioClip[] JumpAudioClip;
     [SerializeField] private AudioSource JumpAudioSource;
+    
 
 
-
-    private void Awake() {
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
 
-    void Update() {
+    void Update()
+    {
 
         Attacking();
 
         horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
             rb.AddForce(Vector2.up * 350);
+            Jump();
         }
         isGrounded = Physics2D.OverlapCircle(GroundChecker.position, 0.2f, GroundLayer);
 
@@ -52,32 +56,49 @@ public class BasicMovimentPlayer : MonoBehaviour {
         animator.SetBool(movemntingHash, horizontalInput != 0);
         animator.SetBool(jumpingHash, !isGrounded);
 
-
-        if (horizontalInput > 0) {
-            spriteRenderer.flipX = false;
-        } else if (horizontalInput < 0) {
-            spriteRenderer.flipX = true;
+        if (horizontalInput > 0 && !facingRight)
+        {
+            Flip();
         }
+        else if (horizontalInput < 0 && facingRight)
+        {
+            Flip();
+        }
+
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
     }
 
 
-    private void Attacking() {
-        if (Input.GetMouseButtonDown(0)) {
+    private void Attacking()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             animator.SetTrigger(attackingHash);
+            
         }
     }
 
 
-    private void Steps() {
+    private void Steps()
+    {
         stepsAudioSource.PlayOneShot(stepsAudioClip[Random.Range(0, stepsAudioClip.Length)]);
     }
 
-    private void Jump() {
+    private void Jump()
+    {
 
         JumpAudioSource.PlayOneShot(JumpAudioClip[Random.Range(0, JumpAudioClip.Length)]);
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
